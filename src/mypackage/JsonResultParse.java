dfc7270e1939f0545970d12e993f27a866bfa506
lsecond <jiang.liu@thescore.com>
2017-04-05 21:33:57 -0400

    Add write to a file

--- src/mypackage/JsonResultParse.java
+++ src/mypackage/JsonResultParse.java
@@ -1,6 +1,8 @@
 package mypackage;
 
+import java.io.BufferedWriter;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.lang.reflect.Array;
 import java.text.ParseException;
@@ -10,12 +12,14 @@ import java.util.Map;
 import java.util.SortedMap;
 import java.util.TreeMap;
 
+import javax.print.attribute.standard.OutputDeviceAssigned;
+
 import com.google.gson.JsonObject;
 import com.google.gson.stream.JsonReader;
 
 public class JsonResultParse{
 	
-	public void readSuites(JsonReader jsonReader) throws IOException{
+	public void readSuites(JsonReader jsonReader, BufferedWriter writer) throws IOException{
 		
 		int blockCount = 0;
 		int overTimeCount = 0;
@@ -25,7 +29,9 @@ public class JsonResultParse{
 		
     	String node = jsonReader.nextName();
     	if (node.equals("suite_name")){
-            System.out.println("Test suites : " + jsonReader.nextString());
+    		String testSuite = jsonReader.nextString();
+            System.out.println("Test suites : " + testSuite);
+            writer.write("Test suites : " +testSuite + "\n");
         } 
         if(node.equals("results")){
         	 jsonReader.beginArray();
@@ -75,13 +81,17 @@ public class JsonResultParse{
         	System.out.println("Pass test: " + passMap.size());
         	for(Map.Entry<String,String> entry : passMap.entrySet()) {
         		System.out.println(entry.getKey() + " runs " + entry.getValue() + " seconds");
+        		writer.write(entry.getKey() + " runs " + entry.getValue() + " seconds"+ "\n");
         	}
         	System.out.println("Failed test: " + failMap.size());
         	for(Map.Entry<String,String> entry : failMap.entrySet()) {
         		System.out.println(entry.getKey() + " runs " + entry.getValue() + " seconds");
+        		writer.write(entry.getKey() + " runs " + entry.getValue() + " seconds"+ "\n");
         	}
         	System.out.println("Block test: " + blockCount);
+        	writer.write("Block test: " + blockCount+ "\n");
         	System.out.println("Over 10 seconds test: " + overTimeCount);
+        	writer.write("Over 10 seconds test: " + overTimeCount+ "\n");
         }
 	}
 	
@@ -91,6 +101,7 @@ public class JsonResultParse{
 	 */
 	public static void main(String [] args) throws IOException{
 		JsonResultParse jsonResultParse = new JsonResultParse();
+		BufferedWriter writer = new BufferedWriter( new FileWriter("output.txt"));
 		JsonReader jsonReader = new JsonReader(new FileReader("test_results.json"));
 	    jsonReader.beginObject();
 	    jsonReader.nextName();
@@ -98,13 +109,15 @@ public class JsonResultParse{
 	    while(jsonReader.hasNext()){
 	    	jsonReader.beginObject();
 		    while (jsonReader.hasNext()) {
-		    	jsonResultParse.readSuites(jsonReader);
+		    	jsonResultParse.readSuites(jsonReader,writer);
 		    }
 		    System.out.print("\n");
+		    writer.write("\n");
 		    jsonReader.endObject();
 	   }
 	   jsonReader.endArray();
 	   jsonReader.endObject();
 	   jsonReader.close();
+	   writer.close();
 	}
 }
